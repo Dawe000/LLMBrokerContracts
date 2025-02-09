842bc849a62fb4d1e996c989adc444550a904761
rruuffuuss <rufusponniah@icloud.com>
2025-02-09 01:09:29 +0000

    NatSpec documentation

--- contracts/LLMBroker.sol
+++ contracts/LLMBroker.sol
@@ -12,7 +12,7 @@ interface ILLMServer {
     function tokenCost() external view returns (uint);
     function updateIndex(uint) external;
 }*/
-
+ 
 contract LLMBroker {
     
     struct Server {
@@ -28,6 +28,7 @@ contract LLMBroker {
 
     Server[] public market;
     
+    
     modifier onlyServer(uint256 index) {
         require(
             market[index].serverContract == msg.sender,
@@ -36,8 +37,11 @@ contract LLMBroker {
         _;
     }
 
+    /// @param serverAddress address of newly created server
+    /// @param owner address of the server owner
     event serverCreated(address indexed serverAddress, address indexed owner);
 
+    /// @notice creates a new server owned by the message sender
     function createServer() external returns (address){
         LLMServer server = new LLMServer(payable(msg.sender), address(this), uint32(market.length));
 
@@ -54,24 +58,32 @@ contract LLMBroker {
         return address(server);
     }
 
-    //delete an item by moving the last item into its index, then popping last item
+    /// @notice lets a server delete itself
+    /// @param index index of the server that is deleting itself
     function deleteServer(uint32 index) external onlyServer(index) {
+        //delete an item by moving the last item into its index, then popping last item
         market[index] = market[market.length - 1];
         market.pop();
         LLMServer server = LLMServer(market[index].serverContract);
         server.updateIndex(index);
     }
 
-    //updates the server details in the market array by reading details from the server contract itself
+    /// @notice update the struct of a server with fresh values from the server contract
+    /// @param index index of the server to be updated
     function updateServerDetails(uint32 index) external {
 
+        //updates the server details in the market array by reading details from the server contract itself
         LLMServer server = LLMServer(market[index].serverContract);
         market[index].model = server.model();
         market[index].inputTokenCost = server.getInputTokenCost();
         market[index].outputTokenCost = server.getOutputTokenCost();
     }
 
-    //updates the server details with arguments when server is sender
+    /// @notice lets a server contract update its own struct with argument values
+    /// @param index index of the server to be updated
+    /// @param _model large language model that the server will use
+    /// @param _inputTokenCost cost of an input token cost in wei
+    /// @param _outputTokenCost cost of an output token in wei
     function updateServerDetails(uint32 index, string calldata _model, uint256 _inputTokenCost, uint256 _outputTokenCost) external onlyServer(index){
 
         market[index].model = _model;
@@ -79,13 +91,17 @@ contract LLMBroker {
         market[index].outputTokenCost = _outputTokenCost;
     }
 
-
+    /// @notice lets a server contract update its own struct with argument values
+    /// @param index index of the server to be updated
+    /// @param _inputTokenCost cost of an input token cost in wei
+    /// @param _outputTokenCost cost of an output token in wei
     function updateServerTokenCost(uint32 index, uint256 _inputTokenCost, uint256 _outputTokenCost) external onlyServer(index){
 
         market[index].inputTokenCost = _inputTokenCost;
         market[index].outputTokenCost = _outputTokenCost;
     }
 
+    /// @notice returns the array of servers
     function getAllServers() external view returns (Server[] memory){
         return market;
     }
